7a66e7f58498789ae16544a4390c1d4d3de5bec1
mateusz <mateusz.wierzbiicki@gmail.com>
2021-08-25 17:22:17 +0200

    dynamic cache with versioning - cache first with network fallback

--- public/sw.js
+++ public/sw.js
@@ -1,48 +1,44 @@
 const self = this;
+const STATIC_FILES_TO_CACHE = [
+  '/',
+  '/static/js/main.chunk.js',
+  '/public/index.html',
+  '/static/js/vendors~main.chunk.js',
+  '/static/js/bundle.js',
+];
+const STATIC_CACHE = 'static-1';
+const DYNAMIC_CACHE = 'dynamic-2';
 
 self.addEventListener('install', (e) => {
   console.log('[Service worker] installed', e);
   e.waitUntil(
-    caches.open('static')
+    caches.open(STATIC_CACHE)
       .then(cache => {
-        // eslint-disable-next-line no-console
-        console.log('Precaching');
-        cache.addAll([
-          '/',
-          '/static/js/main.chunk.js',
-          '/public/index.html',
-          '/static/js/vendors~main.chunk.js',
-          '/static/js/bundle.js',
-        ]);
+        cache.addAll(STATIC_FILES_TO_CACHE);
       })
   )
-})
-
-self.addEventListener('activate', e => {
-  // eslint-disable-next-line no-console
-  console.log('[Service worker] activated', e);
 })
 
 self.addEventListener('fetch', e => {
-  // eslint-disable-next-line no-console
-  console.log('[Service worker] fetch', e);
-  e.respondWith(
-    caches.match(e.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(e.request)
-          .then(res => {
-            return caches.open('dynamic')
-              .then(cache => {
-                cache.put(e.request.url, res.clone());
-                return res;
-              })
-          })
-          .catch(e => {
+  if (e.request.method === 'GET') {
+    e.respondWith(
+      caches.match(e.request)
+        .then(response => {
+          if (response) {
+            return response;
+          }
+          caches.open(DYNAMIC_CACHE)
+            .then(cache => {
+              return fetch(e.request)
+                .then(res => {
+                  cache.put(e.request, res.clone());
+                  return res;
+                })
+            })
+        })
+    )
+  } else {
+    e.respondWith(fetch(e.request))
+  }
 
-          })
-      })
-  )
 })
